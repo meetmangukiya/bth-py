e2fa3db5518cb508f64e32153a0ef9f2395c3c76
Meet Mangukiya <meetmangukiya98@gmail.com>
2019-05-28 19:06:47 +0530

    Improve README and cli help
    Closes https://github.com/meetmangukiya/bth-py/issues/5

--- README.md
+++ README.md
@@ -3,3 +3,28 @@
 Supposed to be like the upwork time tracker i.e. a time tracking application
 for your freelance projects that are not on upwork but when you'd like to
 produce some proof for your employer.
+
+## Installation
+
+1. Install from source
+    `pip install -e .`
+2. Install from PyPI
+    `pip install bth`
+
+## Usage
+
+```
+Usage: bth [OPTIONS] COMMAND [ARGS]...
+
+Options:
+  --help  Show this message and exit.
+
+Commands:
+  ls      Shows the list of all current projects.
+  new     Creates a new project.
+  show    Show work sessions of a particular project.
+  start   Start a new work session on a given project.
+  status  Shows the status, whether working or not? If working, how long
+          has...
+  stop    Stop current work session.
+```
--- bth/cli.py
+++ bth/cli.py
@@ -20,17 +20,27 @@ def command(fn):
 @command
 @click.argument('name')
 def new(name):
+    """
+    Creates a new project.
+    """
     db.add_project(name)
     print(f'Project {name} created successfully')
 
 @command
 def ls():
+    """
+    Shows the list of all current projects.
+    """
     print('ID\tName')
     for name, idd in db.get_projects():
         print(f'{idd}\t{name}')
 
 @command
 def status():
+    """
+    Shows the status, whether working or not? If working, how long has the
+    current sessiono been.
+    """
     pid, sid = db.get_currently_active() or (None, None)
     if pid:
         name, pid = db.get_project(pid)
@@ -43,6 +53,9 @@ def status():
 @command
 @click.argument('idd')
 def start(idd):
+    """
+    Start a new work session on a given project.
+    """
     status = db.start(idd)
     if status:
         print(f'Project {idd} started successfully')
@@ -51,12 +64,18 @@ def start(idd):
 
 @command
 def stop():
+    """
+    Stop current work session.
+    """
     pid, sid = db.stop()
     print(f'Session {sid} of project {pid} stopped successfully')
 
 @command
 @click.argument('idd')
 def show(idd):
+    """
+    Show work sessions of a particular project.
+    """
     print('SID\tStart\t\t\t\tEnd')
     sessions = db.get_session(pid=idd)
     for start, end, pid, sid in sessions:
